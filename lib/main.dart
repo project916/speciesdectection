337d3f5f7010dc0d7c87ce0e1f4228c7b08e7d17
project916 <pranavj10711>
2024-12-06 20:03:15 +0530

    firebase3

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:speciesdectection/Admin/Screen/Admin_home.dart';
 import 'package:speciesdectection/Admin/Screen/Admin_login.dart';
 import 'package:speciesdectection/detection%20and%20processing/screens/Homepage.dart';
+import 'package:speciesdectection/detection%20and%20processing/screens/Registration_screen.dart';
 import 'package:speciesdectection/detection%20and%20processing/screens/login_screen.dart';
 import 'package:speciesdectection/firebase_options.dart';
 
@@ -11,5 +12,5 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home:Homepage()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home:Signup()));
 }
