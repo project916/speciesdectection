5c0683ec42fd3e8169d158e5649a06f2b21e5f3c
project916 <pranavj10711@gmail.com>
2024-12-06 20:41:32 -0900

    firebase6

--- lib/detection and processing/Service/UserAuthService.dart
+++ lib/detection and processing/Service/UserAuthService.dart
@@ -1,32 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-class UserAuthService{
-    final firebaseAuth = FirebaseAuth.instance;
-    final firestoreDatabase=FirebaseFirestore.instance;
-    Future<void> UserRegister({
-      required String name,
+
+class UserAuthService {
+  final firebaseAuth = FirebaseAuth.instance;
+  final firestoreDatabase = FirebaseFirestore.instance;
+  Future<void> UserRegister(
+      {required String name,
       required String email,
       required String mobile,
       required String password,
       required BuildContext context}) async {
-      try{
-        final user= await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password); 
-        
-        firestoreDatabase.collection("Users").doc(user.user?.uid).set({
+    try {
+      final user = await firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
 
-          "name" :name,
-          "email":email,
-          "mobile":mobile,
-          "password":password
+      firestoreDatabase
+          .collection("Users")
+          .doc(user.user?.uid)
+          .set({"name": name, "email": email, "mobile": mobile, });
 
-
-        });
-        
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text("Registration successfull")));
-      }catch(e){
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text("Registration Failed")));
-      }
-
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Registration successfull")));
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Registration Failed")));
     }
+  }
 }
